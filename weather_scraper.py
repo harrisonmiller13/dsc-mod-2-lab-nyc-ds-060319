52405f66573180048b4c90b4d1cce9f9dab0e7c4
“Harrison <harrison.miller13@gmail.com>
2019-06-27 22:04:28 -0400

    updated weather scraper class

--- weather_scraper.py
+++ weather_scraper.py
@@ -17,25 +17,42 @@ class weather_prepare(self)
         # self.df.drop(['Div','FTHG','FTAG','FTR','AwayTeam','HomeTeam','Season'],axis = 1, inplace = True)
         self.df.set_index('Match_ID', inplace = True)
         self.df['Date'] = pd.to_datetime(df['Date'])
-        self.df['Date'] = (
-            df['Date'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+        self.df['unix_Date'] = (df['Date'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
         self.df.sort_values(by = 'Date', inplace = True)
 
 
 
 class weather_scrape(self):
     def __init__(self):
-        self.url = 'https://api.darksky.net/forecast/API_Key/52.52437,13.41053,dates?exclude=minutely,hourly,daily,alerts,flags'
-        self.secret_key = "API_Key"
+        self.url = 'https://api.darksky.net/forecast/'
+        self.key = "API_Key"
         self.berlin_lat = "52.5200"
         self.berlin_long = "13.4050"
         self.url_base = "https://api.darksky.net/forecast"
         self.exclude = 'daily,flags,minutely,hourly,alerts'
-        
-    def api_call(self):
-        payload = {'key':API_Key,'latitude':'52.52437','longitude': '13.41053','time':df['Date'],'exclude': ['minutely','hourly','daily','alerts','flags']}
-        response = requests.get(self.url, params=payload)
-        weather_data = response.json()['currently']['summary']
-        for date in dates:
 
-        return json.dumps(weather_data, indent = 2)
+    def api_call(self,date):
+        
+        payload = {'key':API_Key,'latitude':'52.52437','longitude': '13.41053','time':df['Date'],'exclude': ['minutely','hourly','daily','alerts','flags']}
+        response = requests.get(self.url,params=payload)
+       #dark sky not liking how requests is forming the url query... T-T
+        if response.status_code == 200:
+            return response
+        else:
+            return ValueError
+        weather_data = response.json()['currently']['summary']
+
+    def its_gon_rain(self,weather_data):
+        if weather_data == 'rain':
+            return True
+        else:
+            return False
+
+    def all_the_weather(self,dates):
+        weather_dict = {}
+
+        for date in dates:
+            
+        
+
+
