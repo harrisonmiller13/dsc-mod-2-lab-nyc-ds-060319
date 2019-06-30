f3b3a9c4c5df7b3de26290e402e5c880b26d405b
“Harrison <harrison.miller13@gmail.com>
2019-06-30 10:13:15 -0400

    updated class functions working!

--- weather_scraper.py
+++ weather_scraper.py
@@ -8,20 +8,19 @@ import json
 from apikey import *
 
 
-class weather_prepare(self):
+class weather_prepare:
     def __init__(self):
         self.df = df
 
     # prepare date for use in dark sky api -> convert to unix time
     def prepare_dataframe_for_weather(self):
         
-        self.df.set_index('Match_ID', inplace = True)
-        self.df['Date'] = pd.to_datetime(self.df['Date'])
-        self.df['Unix_Date'] = (self.df['Date'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+        df.set_index('Match_ID', inplace = True)
+        df['Date'] = pd.to_datetime(df['Date'])
+        df['Unix_Date'] = (df['Date'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
     
 
-
-class weather_scrape(self):
+class weather_scrape:
     def __init__(self):
         self.url = 'https://api.darksky.net/forecast'
         self.key = API_Key
@@ -33,7 +32,7 @@ class weather_scrape(self):
     
         good_url = "{}/{}/{},{},{}?exclude={}".format(self.url,self.key,self.latitude, self.longitude, date, self.exclude)
         
-        response = requests.get(self.good_url)
+        response = requests.get(good_url)
         print(response)
         weather = response.json()['daily']['data'][0]['icon']
         return weather
@@ -48,12 +47,13 @@ class weather_scrape(self):
             
 
     def adding_weather_to_df(self):
-        weather_dict = get_all_weather(self, self.df.Unix_Date)
+        weather_dict = get_all_weather(self, df.Unix_Date)
         wdf = pd.DataFrame(weather_dict, index = ['weather'])
         wdf2 = pd.DataFrame.transpose(wdf)
         wdf2 = wdf2.reset_index()
         wdf2.rename(columns={'index': 'Unix_Date'}, inplace=True)
-        pd.merge(df, wdf4, how = 'left', on = 'Unix_Date')
+        pd.merge(df, wdf2, how = 'left', on = 'Unix_Date')
+        return df.head()
 
         
 
